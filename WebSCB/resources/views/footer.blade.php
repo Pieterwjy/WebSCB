8a2e8b4b7aa12c6702790205be2def576bb56428
Pieterwjy <pieterwjy@gmail.com>
2023-06-17 00:43:02 +0700

    Initial Commit

---
+++ WebSCB/resources/views/footer.blade.php
@@ -0,0 +1,92 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+<!-- Footer -->
+<footer class="text-center text-lg-start bg-light text-muted">
+  <!-- Section: Social media -->
+  <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+    <!-- Left -->
+    <div class="me-5 d-none d-lg-block">
+      <span>Get connected with us on social networks:</span>
+    </div>
+    <!-- Left -->
+
+    <!-- Right -->
+    <div>
+      <a href="https://www.youtube.com/c/SCBKemahRajawali" class="me-4 text-reset">
+        <i class="bi bi-youtube"></i>
+      </a>
+      <a href="https://www.facebook.com/groups/157704941450962" class="me-4 text-reset">
+        <i class="bi bi-facebook"></i>
+      </a>
+      <a href="https://www.instagram.com/scbkemahrajawali/" class="me-4 text-reset">
+        <i class="bi bi-instagram"></i>
+      </a>
+    </div>
+    <!-- Right -->
+  </section>
+  <!-- Section: Social media -->
+
+  <!-- Section: Links  -->
+  <section class="">
+    <div class="container text-center text-md-start mt-5">
+      <!-- Grid row -->
+      <div class="row mt-3">
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+          <!-- Content -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            <i class="fas fa-gem me-3"></i>Surabaya City Blessing
+          </h6>
+          <p>
+            Here you can use rows and columns to organize your footer content. Lorem ipsum
+            dolor sit amet, consectetur adipisicing elit.
+          </p>
+        </div>
+       
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">
+            Announcement
+          </h6>
+          <p>
+            <a href="#!" class="text-reset">Pricing</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Settings</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Orders</a>
+          </p>
+          <p>
+            <a href="#!" class="text-reset">Help</a>
+          </p>
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+          <!-- Links -->
+          <h6 class="text-uppercase fw-bold mb-4">Contact</h6>
+          <p><i class="fas fa-home me-3"></i> JL. Satelit Utara 5 No. 18-19, Surabaya</p>
+          <p>
+            <i class="fas fa-envelope me-3"></i>scbkemahrajawali@google.com
+          </p>
+          <p><i class="fas fa-phone me-3"></i> (031) 734 0540</p>
+          <p><i class="fas fa-print me-3"></i> (031) 734 0576</p>
+        </div>
+        <!-- Grid column -->
+      </div>
+      <!-- Grid row -->
+    </div>
+  </section>
+  <!-- Section: Links  -->
+
+  <!-- Copyright -->
+  <div class="text-center p-4 text-light bg-dark" style="background-color: rgba(0, 0, 0, 0.1);">
+    © 2022 Copyright:
+    <a class="text-reset fw-bold" href="/">Surabaya City Blessing</a>
+  </div>
+  <!-- Copyright -->
+</footer>
+<!-- Footer -->
